1c229cac9ff9223cdacd0f2b811710721ac0e0ec
加藤　優奈 <yuvernacrew@gmail.com>
2020-07-18 20:15:42 +0900

    add: 仮のデータを作成

---
+++ assets/data.js
@@ -0,0 +1,67 @@
+const liveList = [
+  {
+    title: 'UVERworld ライブ',
+    artist: 'UVERworld',
+    date: new Date('2017/11/27 20:30'),
+    place: '家',
+    comment: 'だいすき',
+    setList: [
+      {
+        title: 'the over',
+        comment: 'すき',
+      },
+      {
+        title: '心とココロ',
+        comment: 'すき',
+      },
+      {
+        title: 'shout love',
+        comment: 'すき',
+      },
+    ],
+  },
+  {
+    title: 'UVERworld ライブ2',
+    artist: 'UVERworld',
+    date: new Date('2017/11/27 20:30'),
+    place: '家',
+    comment: 'だいすき',
+    setList: [
+      {
+        title: 'the over',
+        comment: 'すき',
+      },
+      {
+        title: '心とココロ',
+        comment: 'すき',
+      },
+      {
+        title: 'shout love',
+        comment: 'すき',
+      },
+    ],
+  },
+  {
+    title: 'UVERworld ライブ3',
+    artist: 'UVERworld',
+    date: new Date('2017/11/27 20:30'),
+    place: '家',
+    comment: 'だいすき',
+    setList: [
+      {
+        title: 'the over',
+        comment: 'すき',
+      },
+      {
+        title: '心とココロ',
+        comment: 'すき',
+      },
+      {
+        title: 'shout love',
+        comment: 'すき',
+      },
+    ],
+  },
+];
+
+export default liveList;
