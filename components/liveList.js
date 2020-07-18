95b3d0b97a6d9844e611b902226c84f6f16bed61
加藤　優奈 <yuvernacrew@gmail.com>
2020-07-18 20:33:52 +0900

    update: スタイルの調整

--- components/liveList.js
+++ components/liveList.js
@@ -6,36 +6,61 @@ import Moment from 'moment';
 const styles = StyleSheet.create({
   container: {
     width: '100%',
-    padding: 20,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
     borderColor: '#3C3C3C',
     borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   title: {
     fontSize: 20,
-    marginBottom: 10,
+    marginBottom: 8,
   },
   detailContainer: {
     flexDirection: 'row',
+    borderColor: '#3C3C3C',
   },
   detail: {
     flexDirection: 'row',
     alignItems: 'center',
+    marginRight: 12,
   },
+  icon: {
+    marginRight: 4,
+  },
+  arrow: {},
 });
 
 const ListItem = ({title, artist, date}) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
-      <View style={styles.detailContainer}>
-        <View style={styles.detail}>
-          <MaterialIcons name="person" size={24} color="gray" />
-          <Text style={styles.artist}>{artist}</Text>
+      <View>
+        <Text style={styles.title}>{title}</Text>
+        <View style={styles.detailContainer}>
+          <View style={styles.detail}>
+            <MaterialIcons
+              style={styles.icon}
+              name="person"
+              size={20}
+              color="gray"
+            />
+            <Text style={styles.artist}>{artist}</Text>
+          </View>
+          <View style={styles.detail}>
+            <MaterialIcons
+              style={styles.icon}
+              name="access-time"
+              size={20}
+              color="gray"
+            />
+            <Text style={styles.date}>{Moment(date).format('YYYY/MM/DD')}</Text>
+          </View>
         </View>
-        <View style={styles.detail}>
-          <MaterialIcons name="access-time" size={24} color="gray" />
-          <Text style={styles.date}>{Moment(date).format('YYYY/MM/DD')}</Text>
-        </View>
+      </View>
+      <View style={styles.arrow}>
+        <MaterialIcons name="chevron-right" size={24} color="gray" />
       </View>
     </View>
   );
